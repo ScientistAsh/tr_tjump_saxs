030f00b5c57d4dc27266d88e02b8e17b0b34a152
Dr. Ashley L. Bennett <ashley.bennett@duke.edu>
2023-04-10 19:13:32 -0400

    1. Added first module file file_handling.py to package 2. Updated make_dir function from CH505TF_SAXS analysis module:     a) Added exception raising and handling     b) Updated docstrings     c) Moved function to file_handling.py module 3. Updated make_flist function from CH505TF_SAXS analysis module:     a) Added exception raising and handling     b) Make accepted delimitter types more generalizable     c) Updated docstrings     d) Moved function to file_handling.py module 4. Updated load_saxs function from CH505TF_SAXS analysis module:     a) Added exception raising and handling     b) Updated docstrings     c) Moved function to file_handling.py module 5. Updated load_set function from CH505TF_SAXS analysis module:     a) Added exception raising and handling     b) Updated docstrings     c) Move function to file_handling.py module

---
+++ file_handling.py
@@ -0,0 +1,467 @@
+'''
+tr_tjump_saxs: file_handle.py
+Date created (mm-dd-yy): 04-01-2023
+
+This module is part of the tr_tjump_saxs package for processing and anlayzing pump-probe time resolved, temperature-jump small angle X-ray scattering data sets. This module includes functions for creating new output directories, loading saxs curves, and plotting saxs curves.
+
+Ashley L. Bennett, PhD
+@ScientistAsh
+'''
+
+# import modules
+import numpy as np
+import scipy
+#from scipy import integrate
+#from scipy.stats import bootstrap
+#from scipy.stats import norm
+import os 
+import pandas as pd
+from pandas import read_table,DataFrame
+from collections import namedtuple
+#from os import listdir, makedirs, path
+import shutil
+#from sys import argv
+import math
+#import pickle as pkl
+#import pprint as pprint
+import warnings
+import matplotlib
+#matplotlib.use("MacOSX")
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import (inset_axes, InsetPosition,
+                                                  mark_inset)
+from numpy.linalg import svd
+#import scipy.stats.mstats
+import sys
+from solution_scattering import *
+import trace
+import parse
+import seaborn as sns
+import matplotlib.pylab as pl 
+from time import sleep
+from tqdm.notebook import tqdm
+import csv
+from sklearn.metrics import r2_score
+from scipy.interpolate import interp1d
+
+
+def make_dir(f):
+    '''
+    Function to assess if a directory exists. If directory does not exists, it will be made.
+    If prompted to enter a new directory path, do use without using quotation marks to inclose
+    the file name.
+    
+    Parameters:
+    -----------
+    f : str 
+        Directory name including full path
+        
+    Returns:
+    --------
+    
+    
+    Raises:
+    -------
+    TypeError
+        When f is not a str type
+        
+    Examples:
+    ---------
+    make_dir(f='./ANALYSIS/')
+    
+    
+    
+    '''
+    try:
+        if not isinstance(f, str):
+            raise TypeError('Oops! f must be str type.  Try again...')
+        
+        elif not os.path.exists(f):
+            os.makedirs(f)
+        else:
+            print('Directory already exists!')
+            
+    except TypeError:
+        new_f = input('A TypeError occurred. Enter a new directory path (do not include quotations): ')
+        make_dir(new_f)
+        
+    return
+
+def make_flist(directory='./', prefix=None, suffix=None):
+    '''
+    Function to create a list of files stored in a specific directory. The first dimension will slice
+    through the files in the list while the second dimension will slice the individual file name. Files
+    are not sorted in any partciular order. 
+    
+    Parameters:
+    -----------
+    directory (optional) : str
+        Directory with files to add to list. Returns a file list. 
+        
+    prefix (optional) : str
+        Beginning of file names. Can be used to limit files appended if not all files in the
+        directory need to be added to the list. When set to None, prefix parameter will not
+        be used to filter the files appended to the file list. Default is None. 
+        
+    suffix (optional) : str
+        Ending of file names. Can be used to limit files appended if not all files in the 
+        directory need to be added to the file list. When set to None, prefix parameter will
+        not be used to filter the files appended to the file list. Default is None.
+        
+    Returns:
+    --------
+    files : list containing the appended files. 
+    
+    Raises:
+    -------
+    TypeError
+        When the given directory, prefix, or suffix is not str type.
+        
+    FileNotFoundError
+        When the given directory does not exist
+        
+    Examples:
+    ----------
+    files = make_flist(directory='/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/',
+                    prefix='diff_TrimerOnly_Series-3_44C_5us_', suffix ='_Q.chi')
+                    
+    files
+    > ['/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_037_-10us_037_Q.chi',
+       '/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_094_-10us_094_Q.chi',
+       '/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_096_-10us_096_Q.chi',
+       '/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_005_-10us_005_Q.chi',
+       '/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_047_-10us_047_Q.chi',
+       '/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_077_-10us_077_Q.chi',
+       '/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_053_-10us_053_Q.chi']
+       
+    files[0]
+    >['/datacommons/dhvi-md/TR_T-jump_SAXS_Mar2023/Trimer10.17Only_Series3_44C/processed_series3_44C_all_images/diff_TrimerOnly_Series-3_44C_5us_037_-10us_037_Q.chi']
+    
+    files[0][-9:-6]
+    > '037'
+    '''
+    
+    try:
+        if not os.path.exists(directory):
+            raise FileNotFoundError('FileNotFoundError: Directory does not exist. Try again...')
+            
+        if not isinstance(directory, str):
+            raise TypeError('TypeError: Oops! directory must be str type. Try again...')
+           
+        if prefix is not None and not isinstance(prefix, str):
+            raise TypeError('TypeError: Oops! prefix must be str type. Try again...')
+            
+        if suffix is not None and not isinstance(suffix, str):
+            raise TypeError('TypeError: Oops! suffix must be str type. Try again...')
+    
+    except (TypeError, FileNotFoundError) as e:
+        print(e.args[0])
+        #print('WARNING: No files loaded')
+        if isinstance(e, TypeError):
+            if e.args[0][17] == 'd':
+                directory = input('Enter a new value for directory (do not include quotations): ')
+                
+            elif e.args[0][17] == 'p':
+                prefix = input('Enter a new value for prefix (do not include quotations): ')
+              
+            elif e.args[0][17] == 's':
+                suffix = input('Enter a new value for suffix (do not include quotations): ')
+                
+        elif isinstance(e, FileNotFoundError):
+            directory = input('Enter a new value for directory (do not include quotations): ')
+            
+        make_flist(directory, prefix, suffix)
+        
+        #return e.args
+        
+
+    # create empty list for files
+    files = []
+
+    # get all files 
+    if prefix is None and suffix is None:
+        for f in tqdm(os.listdir(directory), desc='Getting files'):
+            files.append(str(directory + f))
+            
+    # filter files by suffix
+    elif prefix is None:
+        for f in tqdm(os.listdir(directory), desc='Getting files'):
+            if f.endswith(suffix):
+                files.append(str(directory + f))
+   
+    # filter files by prefix
+    elif suffix is None:
+        for f in tqdm(os.listdir(directory), desc='Getting files'):
+            if f.startswith(prefix):
+                files.append(str(directory + f))
+
+    # filter files by both prefix and suffix
+    elif prefix is not None and suffix is not None:
+        for f in tqdm(os.listdir(directory), desc='Getting files'):
+            if f.startswith(prefix) & f.endswith(suffix):
+                files.append(str(directory + f))
+    
+    print('Done loading ' + str(len(files)) + ' files!')
+    
+    # check that files are loaded into list
+    if len(files) == 0:
+        print('WARNING: No files loaded')
+                        
+    return files
+
+
+
+
+def load_saxs(file, delim=' ', mask=0):
+    '''
+    Function to load a single SAXS difference curve as an array. Data file must be a flat text file
+    delimited by space, tab, or comma. Automatically loads all columns in the given input file. To
+    continue to use the difference curve data for further analysis call the function inside a 
+    variable definition. The first dimension will slice rows and the second will slice columns. 
+    
+    Parameters:
+    -----------
+    
+    file : str
+        File including full path containing SAXS difference curve. File should be a simple space, comma, or 
+        tab delimited text file.
+    
+    delim (optional) : str
+        Type of delimiter used in data file. Accepted values are space (' '), tab ('\t'), and 
+        comma (','). Default value is space (' '). 
+        
+    mask (optional) : int
+        Number of rows to skip when loading files. Default values is 0. Useful for
+        skipping rows with NaN or masked values. 
+
+        
+    Returns:
+    --------
+    data : np.array
+        numpy array with a shape determined by input data. 
+        
+        
+    Raises:
+    -------
+    TypeError 
+        When the file or delim is not str type, mask is not int type, or err is not bool type. 
+        
+    ValueError
+        When the given delim does not match the delimitter in the given file
+        
+    IndexError
+        When errors are indicated but do not exist in the given file
+        
+    Examples:
+    ----------
+    curve = load_saxs(files[0], delim=' ', mask=12)
+    
+    curve
+    > array([[ 0.01947379, -0.03160436],
+            [ 0.02091628, -0.02051868],
+            [ 0.02235877, -0.01261062],
+            ...,
+            [ 2.56431318,  0.00460323],
+            [ 2.56546711,  0.00433085],
+            [ 2.56662079,  0.00426167]])
+    curve[0]
+    > [ 0.01947379, -0.03160436]
+    
+    curve[:, 0]
+    > array([0.01947379, 0.02091628, 0.02235877, ..., 2.56431318, 2.56546711,
+       2.56662079])
+       
+    **(Note that when loading a typical SAXS scattering or difference curve where
+       the first column is scattering vector this slicing yields the scattering vector)
+       
+    curve[:, 1]
+    > array([-0.03160436, -0.02051868, -0.01261062, ...,  0.00460323,
+        0.00433085,  0.00426167]
+        
+    **(Note that when loading a typical SAXS scattering or difference curve where
+       the second column is scattering intensity (difference) this slicing yields the scattering intensity)
+    '''
+    
+    try:
+        if not isinstance(file, str):
+            raise TypeError('TypeError: file must be str type. Try again...')
+            
+        if not isinstance(delim, str):
+            raise TypeError('TypeError: delim must be str type. Try again...')
+        
+        if not isinstance(mask, int):
+            raise TypeError('TypeError: mask must be int type. Try again...')
+
+                
+                
+        data = np.loadtxt(file, delimiter=delim, skiprows=mask)
+                
+                
+                
+    except Exception as e:
+        print(e.args[0])
+        if isinstance(e, TypeError):
+            if e.args[0][11] == 'f':
+                file = input('TypeError: Enter a new str value for file name including full path (do not include quotations): ')
+                
+            elif e.args[0][11] == 'd':
+                delim = input('TypeError: Enter a new str value for delim (do not include quotations): ')
+              
+            elif e.args[0][11] == 'm':
+                mask = int(input('TypeError: Enter a new int value for mask: '))  
+                
+        elif isinstance(e, ValueError):
+            delim = str(input('ValueError: File has a different delim. Enter a new str value for delim (do not include quotations): '))
+        
+    data = np.loadtxt(file, delimiter=delim, skiprows=mask)
+                
+    return data
+
+
+def load_set(flist,  delim=' ', mask=0, err=False):
+    '''
+    Function to load a set of SAXS scattering curves with a specific file prefix/suffix 
+    in a given directory. Input data has columns for q and I.
+    
+    Parameters:
+    -----------
+    flist : list
+        List contining files, with full path, containing SAXS scattering or
+        difference curves curves to run SVD on. Assumes that the input files 
+        contain two columns with the scattering vector (q) and the 
+        scattering intensity (i). If err=True, the the experimental error will
+        also be loaded as long as the input data file contains a 3 column with 
+        the experimental error. 
+        
+    delim (optional) : str
+        Delimitter used in the input data files. Default value is a space (' '). 
+        
+    mask (optional) : int
+        Number of rows to skip when loading files. Default values is 0. Useful for
+        skipping rows with NaN or masked values. 
+        
+    err (optional) : bool
+        Indicates if there is the column containing experimental error. If set 
+        to False, then no errors will be loaded. Default value is False. 
+        
+        
+    Returns:
+    --------
+    data : list 
+        List containing the scattering intensity (i) vector for the loaded curves.
+        The curves are loaded in the same order that are in flist. 
+    
+    data_arr : np.array
+        Numpy array containing the scattering vector (q) and scattering intensity (i).
+        Array has shape (n, r), where n is the number of scattering curves loaded and r is the 
+        number of entries in each loaded curve. 2 represents scattering intensity(i), for which 
+        there are r number of values in each, for each loaded curve n. The curves are loaded in 
+        the same order that are in flist.
+    
+    q :  np.array
+        Numpy array containing scattering vector (q) values. 
+    
+    error : np.array
+        Numpy array containing the experimental error for scattering intensity (i). Will be an 
+        empty array if there is no error column in the imported 
+        
+    Raises:
+    -------
+    TypeError
+        When flist or delim is not str type, mask is not int type, or err is not bool type. 
+        
+    IndexError
+        When a column for error values is indicated but does not exist in the given files. 
+        
+    ValueError
+        When the given delim does not match the delimitter in the given file. 
+        
+
+    Examples:
+    ----------
+    data, data_arr, q, error = load_set(flist=files, delim=' ', mask=12, err=False)
+    
+    for c in data_arr:
+        plt.plot(q, c)
+    
+    '''
+    
+    # initialize list to store data
+    data = []
+    error = []
+    
+    # test if input parameters are proper data type
+    try:
+        if not isinstance(flist, list):     
+            raise TypeError('TypeError: Oops! flist must be list type. Try again...')
+            
+        if not isinstance(delim, str):     
+            raise TypeError('TypeError: Oops! delim must be str type. Try again...')
+        
+        if not isinstance(mask, int):     
+            raise TypeError('TypeError: Oops! mask must be int type. Try again...')
+        
+        if not isinstance(err, bool):     
+            raise TypeError('Oops! err must be bool type. Try again...')
+            
+                    
+        # load laser on data
+        for f in tqdm(flist, desc='Loading curves'):
+            curve = load_saxs(file=f, delim=delim, mask=mask) 
+        
+            # append curve to vector list
+            data.append(curve[:,1])
+        
+            if err == True:
+                if curve.shape[1] < 3:
+                    raise IndexError('File does not contain errors. Change err to False')
+                else:
+                    error.append(curve[:,2])
+            
+            else:
+                continue 
+
+            
+    except Exception as e:
+        print(e.args[0])
+        if isinstance(e, TypeError):
+            if e.args[0][17] == 'f':
+                flist = input('TypeError: Enter a new list value for flist. Include full path and quotations for each file name (do not include quotations around list): ')
+                
+            elif e.args[0][17] == 'd':
+                delim = input('TypeError: Enter a new str value for delim (do not include quotations): ')
+              
+            elif e.args[0][11] == 'm':
+                mask = int(input('TypeError: Enter a new int value for mask: '))  
+                
+            elif e.args[0][17] == 'e':
+                err = input('TypeError: Enter a new bool value for err: ')
+                
+        elif isinstance(e, ValueError):
+            delim = str(input('ValueError: File has a different delim. Enter a new str value for delim (do not include quotations): '))
+            
+        elif isinstance(e, IndexError):
+            print('IndexError: err=True indicates file contains error but there is no column containing errors. Changing err to False...')
+            err = False
+            # load laser on data
+            
+            for f in tqdm(flist, desc='Loading curves'):
+                curve = load_saxs(file=f, delim=delim, mask=mask) 
+        
+                # append curve to vector list
+                data.append(curve[:,1])
+            data.remove(data[0])
+           
+
+    # turn data list into array
+    data_arr = np.array(data)
+    
+    # turn err list into array
+    error = np.array(error)
+    
+    # get q
+    q = curve[:,0]
+    
+    print('Done loading ' + str(len(data)) + ' curves!')
+        
+    return data, data_arr, q, error
